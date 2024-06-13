e67550c9235e58b48d1b15ae3ecbc56e9d9c00df
Karthik S <karthik.s@technoforte.co.in>
2024-06-13 10:09:02 +0530

    componnent scanning outside of spring boot main java application

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/karthi/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.karthi.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.karthi.util.Coach;
+
+@RestController
+
+public class DemoController {
+
+	//private field for the dependency
+	private Coach myCoach;
+	//define constructor for dependency injection
+	@Autowired
+	public DemoController(Coach theCoach) {
+		myCoach = theCoach;
+	}
+	
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return myCoach.getDailyWorkout();
+	}
+	
+}
---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/karthi/util/Coach.java
@@ -0,0 +1,6 @@
+package com.karthi.util;
+
+public interface Coach {
+
+	String getDailyWorkout();
+}
