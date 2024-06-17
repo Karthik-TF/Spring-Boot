8c4450ecb22a5817aba43932596c90978994f82e
Karthik S <karthik.s@technoforte.co.in>
2024-06-17 19:06:53 +0530

    Hibernate/Jpa-Setting up cruddemo Application

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/test/java/com/karthi/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthi.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
