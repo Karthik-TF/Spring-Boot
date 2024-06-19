7ee3980f88497cad3abdb18e1ace6c741180f857
Karthik S <karthik.s@technoforte.co.in>
2024-06-19 11:35:20 +0530

    Creating and saving new student object

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/karthi/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/karthi/cruddemo/CruddemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.karthi.cruddemo.dao.StudentDAO;
+import com.karthi.cruddemo.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -13,10 +16,27 @@ public class CruddemoApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner commandLineRunner(String []args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner ->{
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
 	}
 
+	private void createStudent(StudentDAO studentDAO) {
+		// TODO Auto-generated method stub
+		//create student object 
+		System.out.println("Creating new Student object...");
+		Student tempStudent = new Student("karthik","s","karthi@123.com");
+		
+		//save student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+		//display id of saved student obj
+		System.out.println("saved student. Generated id: "+ tempStudent.getId());
+		
+	}
+
+	
+	
+	
 }
