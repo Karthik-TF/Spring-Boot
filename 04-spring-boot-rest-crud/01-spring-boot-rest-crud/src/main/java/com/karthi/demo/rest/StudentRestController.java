da126626ee34793ad7416029fcb1ed77fc14fde6
Karthik S <karthik.s@technoforte.co.in>
2024-06-24 12:41:42 +0530

    Exception Handling using @ExceptionHandler for path variable student id exceeding list size

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/karthi/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/karthi/demo/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.karthi.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +41,26 @@ public class StudentRestController {
 	//new endpoint for the path variable one to retrieve the student by studentId
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		//check the studentId agains the list size
+		if ((studentId >= theStudents.size())||(studentId<0)) {
+			throw new StudentNotFoundException("Student id not found -"+studentId);
+			
+		}
 		return theStudents.get(studentId);
 	}
-
+	
+	//add an exception handler using @ExceptionHandler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+		//create a studentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		//return the Response entity
+		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);	
+		
+	}
+	
 }
