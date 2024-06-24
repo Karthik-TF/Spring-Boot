f3eb19cb6637a1983ac8a9c4b577afea53487a73
Karthik S <karthik.s@technoforte.co.in>
2024-06-24 13:07:57 +0530

    exception handler for genric exceptions

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/karthi/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/karthi/demo/rest/StudentRestController.java
@@ -62,5 +62,20 @@ public class StudentRestController {
 		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);	
 		
 	}
+	//add another exception handler to catch any exception handling ...catch all
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		//create a studentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+				
+		//return the Response entity
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+		
+	}
+	
+	
 	
 }
