8a56341ec27242d2c0cd1124c2eab87650fa6e77
Karthik S <karthik.s@technoforte.co.in>
2024-06-25 12:47:33 +0530

    Service - Add, Save, Delete

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/service/EmployeeService.java
@@ -6,5 +6,12 @@ import com.karthi.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeService {
 	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	Employee save(Employee theEmployee);
+	
+	void deleteById(int theId);
+
 
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.karthi.springboot.cruddemo.dao.EmployeeDAO;
 import com.karthi.springboot.cruddemo.entity.Employee;
@@ -23,6 +24,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 	public List<Employee> findAll() {
 		// TODO Auto-generated method stub
 		return employeeDAO.findAll();
+	}
+
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return employeeDAO.findById(theId);
+	}
+
+
+	@Transactional
+	@Override
+	public Employee save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+		return employeeDAO.save(theEmployee);
+	}
+
+
+	@Transactional
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		employeeDAO.deleteById(theId);
+		
 	}
 
 }
