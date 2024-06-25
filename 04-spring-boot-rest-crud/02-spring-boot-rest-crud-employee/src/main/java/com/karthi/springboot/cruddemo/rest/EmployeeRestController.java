7324cc5638ea2f7163ab1e3561723fc9547824bf
Karthik S <karthik.s@technoforte.co.in>
2024-06-25 14:46:16 +0530

    Read single employee by path variable endpoint

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/karthi/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,16 @@ public class EmployeeRestController {
 	public List<Employee>findAll(){
 		return employeeService.findAll();
 	}
+	
+	// mapping "/employee/{empployeeId}" for single employee
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int  employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		if(theEmployee==null) {
+			throw new RuntimeException("Employee id not found - "+employeeId);
+		}
+		return theEmployee;
+	}
+	
 
 }
