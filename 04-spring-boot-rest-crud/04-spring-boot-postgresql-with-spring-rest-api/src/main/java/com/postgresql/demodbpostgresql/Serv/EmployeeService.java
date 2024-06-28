a461297e6867de3c607bd3254660dca12464d4a4
Karthik S <karthik.s@technoforte.co.in>
2024-06-28 11:26:45 +0530

    Refactored the code to use Spring Data REST

--- 04-spring-boot-rest-crud/04-spring-boot-postgresql-with-spring-rest-api/src/main/java/com/postgresql/demodbpostgresql/Serv/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.postgresql.demodbpostgresql.Serv;
-
-import java.util.List;
-
-import com.postgresql.demodbpostgresql.entity.Employee;
-public interface EmployeeService {
-	List<Employee> findAll();
-	Employee save(Employee theEmployee);
-}
