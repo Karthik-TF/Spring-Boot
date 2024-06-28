aec50901d34fd4540797ecbe2079171b93419a45
Karthik S <karthik.s@technoforte.co.in>
2024-06-28 12:23:28 +0530

    REST CRUD APIs Over

--- 04-spring-boot-rest-crud/04-spring-boot-postgresql-with-spring-rest-api/src/main/java/com/postgresql/demodbpostgresql/jparepository/EmployeeJpaRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-postgresql-with-spring-rest-api/src/main/java/com/postgresql/demodbpostgresql/jparepository/EmployeeJpaRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.postgresql.demodbpostgresql.entity.Employee;
-@RepositoryRestResource(path="members")
+//@RepositoryRestResource(path="members")
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Integer>{
 	//
 }
